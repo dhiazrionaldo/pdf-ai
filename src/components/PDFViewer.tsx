6bb7dc5b5b2d89ac82daaaf539db12655dd9e946
dhiazrionaldo <dhiazrionaldo@rtechindonesia.com>
2024-04-17 10:49:36 +0700

    change loader condition on pdf viewer and add scroll y to chat side bar

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -1,11 +1,11 @@
 "use client"
-import React , {useState} from "react";
-import {Loader2} from 'lucide-react'
+import React , { useState } from "react";
+import { Loader2 } from 'lucide-react';
 
 type Props = { pdf_url: string };
 
 const PDFViewer = ({ pdf_url }: Props) => {
-  const completeUrl = `https://docs.google.com/gview?url=${pdf_url}&embedded=true`
+  const completeUrl = `https://docs.google.com/gview?url=${pdf_url}&embedded=true`;
   const [isLoading, setIsLoading] = useState(true);
   
   const handleLoad = () => {
@@ -13,18 +13,14 @@ const PDFViewer = ({ pdf_url }: Props) => {
   }
   
   return (
-    <div className="w-full h-full">
-      {isLoading && (
-        // Display loading indicator while loading
-        <div className="w-full h-full flex flex-col items-center text-center">
-          
-          <Loader2 className='h-10 w-10 text-blue-600 animate-spin' />
-          {/* Add optional spinner or progress bar */}
-        </div>
-      )}
+    <div className="w-full h-full relative">
+      {/* Always render loader and iframe, but control their visibility */}
+      <div className={`w-full h-full flex items-center justify-center absolute top-0 left-0 ${isLoading ? 'block' : 'hidden'}`}>
+        <Loader2 className='h-10 w-10 text-blue-600 animate-spin' />
+      </div>
       <iframe
         src={completeUrl}
-        className="w-full h-full"
+        className={`w-full h-full ${isLoading ? 'hidden' : 'block'}`}
         onLoad={handleLoad}
       />
     </div>
