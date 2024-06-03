5de8d20f3bd6b3e2b281741e06708f53a98655cc
dhiaz.rionaldo@gmail.com <dhiazrionaldo@gmail.com>
2024-06-03 12:54:29 +0700

    migrate aws-sdk from v2 to v3

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -1,18 +1,17 @@
-import AWS from 'aws-sdk'
-import fs from 'fs'
+import { S3, GetObjectCommand } from '@aws-sdk/client-s3';
+import fs from 'fs';
+import { Readable } from 'stream';
 
-export async function downloadFromS3(file_key: string){
+export async function downloadFromS3(file_key: string) {
     try {
-        AWS.config.update({
-            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
-            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY
-        });
+        console.log("Initializing S3 client...");
 
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME
-            },
-            region: 'ap-southeast-2'
+        const s3 = new S3({
+            region: 'ap-southeast-2',
+            credentials: {
+                accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY!,
+                secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!
+            }
         });
 
         const params = {
@@ -20,14 +19,67 @@ export async function downloadFromS3(file_key: string){
             Key: file_key
         };
 
-        const obj = await s3.getObject(params).promise();
-        const file_name = `/tmp/pdf-${Date.now()}.pdf`
-        fs.writeFileSync(file_name, obj.Body as Buffer);
+        const command = new GetObjectCommand(params);
 
-        return file_name
+        const obj = await s3.send(command);
+
+        const file_name = `/tmp/pdf-${Date.now()}.pdf`;
+
+        if (obj.Body instanceof Readable) {
+            await streamToFile(obj.Body, file_name);
+        } else if (Buffer.isBuffer(obj.Body)) {
+            fs.writeFileSync(file_name, obj.Body);
+        } else {
+            throw new Error("Unexpected response type");
+        }
+
+        return file_name;
     } catch (error) {
-        console.log(error)
-
-        return null
+        console.error('Error downloading from S3:', error);
+        return null;
     }
 }
+
+function streamToFile(stream: Readable, path: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const writeStream = fs.createWriteStream(path);
+        stream.pipe(writeStream);
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+    });
+}
+
+// import AWS from 'aws-sdk'
+// import fs from 'fs'
+
+// export async function downloadFromS3(file_key: string){
+//     try {
+//         console.log("Initializing S3 client...");
+//         AWS.config.update({
+//             accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
+//             secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY
+//         });
+
+//         const s3 = new AWS.S3({
+//             params: {
+//                 Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME
+//             },
+//             region: 'ap-southeast-2'
+//         });
+
+//         const params = {
+//             Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+//             Key: file_key
+//         };
+
+//         const obj = await s3.getObject(params).promise();
+//         const file_name = `/tmp/pdf-${Date.now()}.pdf`
+//         fs.writeFileSync(file_name, obj.Body as Buffer);
+
+//         return file_name
+//     } catch (error) {
+//         console.log(error)
+
+//         return null
+//     }
+// }
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -1,45 +1,94 @@
-import AWS from 'aws-sdk'
+import { S3, PutObjectCommand } from '@aws-sdk/client-s3';
 
 export async function uploadToS3(file: File){
     try {
-        AWS.config.update({
-            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
-            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY
+        console.log("Initializing S3 client...");
+
+        const s3 = new S3({
+            region: 'ap-southeast-2',
+            credentials: {
+                accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY!,
+                secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!
+            }
         });
 
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME
-            },
-            region: 'ap-southeast-2'
-        })
-
-        const file_key = 'uploads/'+Date.now().toString()+file.name.replace(/\s/g, '-');;
-
+        const file_key = 'uploads/' + Date.now().toString() + file.name.replace(/\s/g, '-');
+        
+        console.log("Preparing upload parameters...");
+        
         const params = {
             Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
             Key: file_key,
             Body: file
-        }
+        };
 
-        const upload = s3.putObject(params).on('httpUploadProgress', evt => {
-            console.log('Uploading to s3...', parseInt(((evt.loaded*100)/evt.total).toString())) + "%"
-        }).promise()
+        console.log("Creating PutObjectCommand...");
 
-        await upload.then(data => {
-            console.log('success uploaded to s3'+file_key);
-        })
+        const command = new PutObjectCommand(params);
+
+        console.log("Sending upload request...");
+
+        await s3.send(command);
+
+        console.log('Successfully uploaded to S3: ' + file_key);
 
         return Promise.resolve({
             file_key,
             file_name: file.name
-        })
+        });
     } catch (error) {
-        
+        console.error('Error uploading to S3:', error);
+        throw error;
     }
 }
 
 export function getS3Url(file_key: string){
     const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-southeast-2.amazonaws.com/${file_key}`;
-    return url
+    return url;
 }
+
+// import AWS from 'aws-sdk'
+
+// export async function uploadToS3(file: File){
+//     try {
+//         AWS.config.update({
+//             accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY,
+//             secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY
+//         });
+
+//         const s3 = new AWS.S3({
+//             params: {
+//                 Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME
+//             },
+//             region: 'ap-southeast-2'
+//         })
+
+//         const file_key = 'uploads/'+Date.now().toString()+file.name.replace(/\s/g, '-');;
+
+//         const params = {
+//             Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+//             Key: file_key,
+//             Body: file
+//         }
+
+//         const upload = s3.putObject(params).on('httpUploadProgress', evt => {
+//             console.log('Uploading to s3...', parseInt(((evt.loaded*100)/evt.total).toString())) + "%"
+//         }).promise()
+
+//         await upload.then(data => {
+//             console.log('success uploaded to s3'+file_key);
+//         })
+
+//         return Promise.resolve({
+//             file_key,
+//             file_name: file.name
+//         })
+//     } catch (error) {
+        
+//     }
+// }
+
+// export function getS3Url(file_key: string){
+//     const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-southeast-2.amazonaws.com/${file_key}`;
+//     return url
+// }
