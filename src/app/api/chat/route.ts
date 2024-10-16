0e7a7f1004b273bdcde727f02c33c5c04e76def1
dhiaz.rionaldo@gmail.com <dhiazrionaldo@gmail.com>
2024-10-16 08:20:14 +0700

    change AI model to test accuracy

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -71,7 +71,7 @@ export async function POST(req: Request) {
     };
     
     const response = await openai.createChatCompletion({
-      model: "gpt-4-turbo",
+      model: "gpt-4o",
       messages: [
         prompt,
         ...messages.filter((message: Message) => message.role === "user"),
